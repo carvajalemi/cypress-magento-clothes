cebce4f1b3266c9a86e80336fe87f664d193b800
Sheilasc <sheila.salinas@gmail.com>
2023-02-16 20:42:51 -0400

    Adding three test cases

--- cypress/e2e/luma-Women-Bottoms-Pants-AddToCart-Verify that any product can be added to the cart.cy.js
+++ cypress/e2e/luma-Women-Bottoms-Pants-AddToCart-Verify that any product can be added to the cart.cy.js
@@ -1,5 +1,5 @@
-describe('Test Women-Bottoms-Pants the images of the pages are rotated after clicking on the arrows ', () => {
-  it('It should validate Women-Bottoms-Pants the images of the pages are rotated after clicking on the arrows', () => {
+describe('Test Women-Bottoms-Pants-AddToCart-Verify that any product can be added to the cart', () => {
+  it('It should validate Women-Bottoms-Pants-AddToCart-Verify that any product can be added to the cart', () => {
     //cy.visit(Cypress.env('baseUrls'))
     cy.visit('https://magento.softwaretestingboard.com')
 
@@ -17,14 +17,29 @@ describe('Test Women-Bottoms-Pants the images of the pages are rotated after cli
     
     //get one item and click on it
     cy.get(':nth-child(1) > .product-item-info > .photo > .product-image-container > .product-image-wrapper > .product-image-photo').click()
-
+    
     //first image is displayed
     cy.get('[src="https://magento.softwaretestingboard.com/pub/media/catalog/product/cache/4eb2c46e52caa44ad0c1a1e7a8bca863/w/p/wp13-orange_main_1.jpg"]').should('be.visible')
-   
-    //click on arrow to see the second image
-    cy.get('.fotorama__arr--next > .fotorama__arr__arr').click()
+    cy.wait(5000)
 
-    //second image is displayed
-    cy.get('[src="https://magento.softwaretestingboard.com/pub/media/catalog/product/cache/4eb2c46e52caa44ad0c1a1e7a8bca863/w/p/wp13-orange_back_1.jpg"]').should('be.visible')
+    // get Size and click on it
+    cy.get('#option-label-size-143-item-171').click()
+
+    //get Color and click on it
+    cy.get('#option-label-color-93-item-50').click()
+
+    //get the 'Add to cart' button and click on it
+    cy.get('#product-addtocart-button').click()
+
+    //verify the message is displayed
+    cy.get('.message-success').should('be.visible')
+
+    //get the cart
+    cy.get('.showcart > .counter').click()
+
+    //verify the item is in the cart
+    cy.get('.minicart-items-wrapper').should('be.visible')
+
+
   })
 })
