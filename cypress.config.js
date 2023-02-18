9e31320f662bce70b27e27c9eb66b60f96d42dfb
Emilia Carvajal <emilia.carvajalm@gmail.com>
2023-02-17 23:29:13 -0400

    Adding changes in the config

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  env: {
+    baseUrls: "https://magento.softwaretestingboard.com/",
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
